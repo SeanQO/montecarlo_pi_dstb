ab7b2b569ed9cdd1d1edd0d0d1c3344e6b313ed0
StivenArboleda <stiven2201@gmail.com>
2022-06-10 03:10:45 -0500

    Finish callback impl

--- worker/src/main/java/Main_Worker.java
+++ worker/src/main/java/Main_Worker.java
@@ -11,24 +11,24 @@ public class Main_Worker {
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.client"))
         {
             
+
+            com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Worker.Server");
+            adapter.add(new Slave(), com.zeroc.Ice.Util.stringToIdentity("cliente"));
+            adapter.activate();
+            
             intfc.ServerPrx serverPrx = ServerPrx.checkedCast(
             communicator.propertyToProxy("Server.Proxy")).ice_twoway().ice_timeout(-1).ice_secure(false);
             if(serverPrx == null)
             {
                 System.err.println("invalid proxy");
             }
-            
-            serverPrx.
 
-            com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Worker.Server");
-            adapter.add(new Slave(), com.zeroc.Ice.Util.stringToIdentity("cliente"));
-            adapter.activate();
             
             WorkerPrx workerPrx = WorkerPrx.uncheckedCast(adapter.createProxy(
             com.zeroc.Ice.Util.stringToIdentity("cliente")));
 
 
-            serverPrx.callCallBack(workerPrx);
+            serverPrx.attach(workerPrx);
             
         }
 
